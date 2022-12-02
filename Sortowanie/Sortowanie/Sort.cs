cae55f5a43c642ce9d5190d0fa37106c6de3d7cf
Kuba Bielecki <kuba.bielecki@outlook.com>
2022-12-02 13:17:05 +0100

    sort class upgrade

--- Sortowanie/Sortowanie/Sort.cs
+++ Sortowanie/Sortowanie/Sort.cs
@@ -7,7 +7,7 @@ namespace Sortowanie
     {
         public static void InsertionSort(TextBox input, TextBox output)
         {
-            int[] arrayToSort = inputSplit(input);
+            int[] arrayToSort = InputSplit(input);
 
             for (int i = 1; i < arrayToSort.Length; i++)
             {
@@ -23,23 +23,28 @@ namespace Sortowanie
                 arrayToSort[j + 1] = temp;
             }
 
-            string stringOutput = "";
-
-            for (int i = 0; i < arrayToSort.Length; i++)
-            {
-                stringOutput += arrayToSort[i].ToString() + ", ";
-            }
-
-            output.Text = stringOutput;
+            WriteOutput(arrayToSort, output);
         }
 
-        static int[] inputSplit(TextBox input)
+        static int[] InputSplit(TextBox input)
         {
             string stringArr = input.Text;
 
             int[] intArr = stringArr.Split(',').Select(int.Parse).ToArray();
 
             return intArr;
+        }
+
+        static void WriteOutput(int[] arr, TextBox output)
+        {
+            string stringOutput = "";
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                stringOutput += arr[i].ToString() + ", ";
+            }
+
+            output.Text = stringOutput;
         }
     }
 }
